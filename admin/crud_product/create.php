7310a80e1ee6a9c6ea3fc2d59f1aeaeb58519038
adultsforfuture <adultsforfuture@gmail.com>
2021-07-11 23:05:17 +0700

    fdsf

--- admin/crud_product/create.php
+++ admin/crud_product/create.php
@@ -16,7 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Dashboard - Admin</title>
+    <title>Dashboard - SB Admin</title>
     <link href="../../css/styles.css" rel="stylesheet" />
 </head>
 
