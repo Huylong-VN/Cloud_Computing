b2834cca06062af1e2e83174d32b200a9e3e14a7
Anh Huy <73161051+Huylong-VN@users.noreply.github.com>
2021-07-12 06:27:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,52 +7,58 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./bootstrap-5.0.2-dist/css/bootstrap.min.css">
 </head>
 
 <body>
     <?php session_start(); ?>
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <a class="navbar-brand" href="index.php">ATN</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false" aria-label="Toggle navigation">More</button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <?php 
-                if (isset($_SESSION['username'])) {
-                    echo ("
-                    <li class='nav-item'>
-                    <a class='nav-link' href='#'>Xin Chào : " . $_SESSION['username'] . "</a>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">ATN</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                     </li>
-                    <li class='nav-item'>
-                    <a class='nav-link' href='cart.php'>Cart <i class='fas fa-cart-plus'></i></a>
-                    </li>
-                    <li class='nav-item'>
-                    <a class='nav-link' href='?logout'>Đăng xuất</a>
-                    </li>
-                    ");
-                }
-                else{
-                    echo("  <li class='nav-item'>
-                    <a class='nav-link' href='login.php'>Đăng nhập</a>
-                    </li>");
-                }
-                if (isset($_GET['logout'])) {
-                    session_destroy();
-                    header("location:index.php");
-                }
-               
-                ?>
-
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+                    <?php 
+                    if (isset($_SESSION['username'])) {
+                        echo ("
+                        <li class='nav-item'>
+                        <a class='nav-link' href='#'>Xin Chào : " . $_SESSION['username'] . "</a>
+                        </li>
+                        <li class='nav-item'>
+                        <a class='nav-link' href='cart.php'>Cart <i class='fas fa-cart-plus'></i></a>
+                        </li>
+                        <li class='nav-item'>
+                        <a class='nav-link' href='?logout'>Đăng xuất</a>
+                        </li>
+                        ");
+                    }
+                    else{
+                        echo("  <li class='nav-item'>
+                        <a class='nav-link' href='login.php'>Đăng nhập</a>
+                        </li>");
+                    }
+                    if (isset($_GET['logout'])) {
+                        session_destroy();
+                        header("location:index.php");
+                    }
+                   
+                    ?>
+    
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
         </div>
+       
     </nav>
     <div class="container">
         <div class="row">
@@ -137,7 +143,7 @@
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="./bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
